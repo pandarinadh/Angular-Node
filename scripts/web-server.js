1014ea3382bc44288a69646b7ac79e99b44b5965
pandarinadh <39387722+pandarinadh@users.noreply.github.com>
2018-06-29 14:52:26 -0400

    Revert "Initial commit"
    This reverts commit 84f4f828f06e53d597702a598dbc61f02934311f.

--- scripts/web-server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var rootpath = path.normalize(__dirname + '/../');
-
-app.use(express.static(rootpath + '/app'));
-
-app.listen(8000);
-console.log('listening on port 8000 ...');
