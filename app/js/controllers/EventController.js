13fab23e7f474a6288233b3e54a4100b38394e36
pandarinadh <39387722+pandarinadh@users.noreply.github.com>
2018-06-29 15:57:11 -0400

    d3 changes

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -52,6 +52,7 @@ eventsApp.controller('EventController', function EventController($scope)
             }
         ]
     }
+
     $scope.drawChart = function(){
         var data = $scope.data;
         var svgWidth = 600, svgHeight = 400;
@@ -66,7 +67,38 @@ eventsApp.controller('EventController', function EventController($scope)
             .attr("transform", 
             "translate(" + margin.left + "," + margin.top + ")");
 
-            var x = d3.scaleTime().rangeRound([0, width]);
+            var x = d3.scaleLinear().rangeRound([0, width]);
+            var y = d3.scaleLinear().rangeRound([height, 0]);
+            
+            var line = d3.line()
+            .x(function(d) { return x(d.x)})
+            .y(function(d) { return y(d.y)})
+            x.domain(d3.extent(data, function(d) { return d.x }));
+            y.domain(d3.extent(data, function(d) { return d.y }));
+
+            g.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x))
+            .select(".domain")
+            .remove();
+
+    }
+
+    $scope.drawChart1 = function(){
+        var data = $scope.data;
+        var svgWidth = 600, svgHeight = 400;
+        var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+        var width = svgWidth - margin.left - margin.right;
+        var height = svgHeight - margin.top - margin.bottom;
+        var svg = d3.select('svg')
+            .attr("width", svgWidth)
+            .attr("height", svgHeight);
+
+            var g = svg.append("g")
+            .attr("transform", 
+            "translate(" + margin.left + "," + margin.top + ")");
+
+            var x = d3.scaleLinear().rangeRound([0, width]);
             var y = d3.scaleLinear().rangeRound([height, 0]);
             
             var line = d3.line()
