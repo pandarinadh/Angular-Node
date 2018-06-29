2ab71cc6b6b2036f100df14df89077ceeff282b9
pandarinadh <39387722+pandarinadh@users.noreply.github.com>
2018-06-29 15:15:57 -0400

    with graph

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -2,6 +2,20 @@
 
 eventsApp.controller('EventController', function EventController($scope)
 {
+    $scope.data =
+    [{
+        x: 1,
+        y: 3
+    },
+    {
+        x: 5,
+        y: 15
+    },
+    {
+        x: 14,
+        y: 25
+    }
+    ]
     $scope.event = {
         name: 'Angular Boot Camp',
         date: '1/1/2018',
@@ -37,6 +51,55 @@ eventsApp.controller('EventController', function EventController($scope)
                 upVoteCount:0
             }
         ]
+    }
+    $scope.drawChart = function(){
+        var data = $scope.data;
+        var svgWidth = 600, svgHeight = 400;
+        var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+        var width = svgWidth - margin.left - margin.right;
+        var height = svgHeight - margin.top - margin.bottom;
+        var svg = d3.select('svg')
+            .attr("width", svgWidth)
+            .attr("height", svgHeight);
+
+            var g = svg.append("g")
+            .attr("transform", 
+            "translate(" + margin.left + "," + margin.top + ")");
+
+            var x = d3.scaleTime().rangeRound([0, width]);
+            var y = d3.scaleLinear().rangeRound([height, 0]);
+            
+            var line = d3.line()
+            .x(function(d) { return x(d.x)})
+            .y(function(d) { return y(d.y)})
+            x.domain(d3.extent(data, function(d) { return d.x }));
+            y.domain(d3.extent(data, function(d) { return d.y }));
+
+            g.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x))
+            .select(".domain")
+            .remove();
+
+                        g.append("g")
+            .call(d3.axisLeft(y))
+            .append("text")
+            .attr("fill", "#000")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", "0.71em")
+            .attr("text-anchor", "end")
+            .text("Price ($)");
+
+                    g.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", line);
+
     }
     $scope.upVoteSession = function(sesion)
     {
