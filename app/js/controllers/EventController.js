9a8b364fe6a970fb0d6475f0c604b6b9bba58c94
pandarinadh <39387722+pandarinadh@users.noreply.github.com>
2018-07-02 11:30:26 -0400

    added d3min to solution

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -5,10 +5,16 @@ angular.element(document).ready(function() {
 app.controller('EventController', function EventController($scope)
 {
     $scope.graph = {
-        'width': 300,
-        'height': 100
+        'width': 600,
+        'height': 300,
+        MARGINS: {
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 50
+          }
     };
-    $scope.chartPoints = [];
+    
     $scope.points =
     [{
         'x': 1,
@@ -19,22 +25,110 @@ app.controller('EventController', function EventController($scope)
         'y': 5
     },
     {
-        'x': 6,
+        'x': 10,
         'y': 10
     }
     ];
+
+    $scope.chartPoints = [
+        {
+            color :  'green',
+        points :  [{
+                'x': 1,
+                'y': 3
+            },
+            {
+                'x': 3,
+                'y': 5
+            },
+            {
+                'x': 10,
+                'y': 10
+            }
+            ]
+        },
+        {
+            color : 'blue',
+            points : [{
+                'x': 2,
+                'y': 6
+            },
+            {
+                'x': 4,
+                'y': 8
+            },
+            {
+                'x': 9,
+                'y': 10
+            }
+            ]
+        }
+
+    ];
+
     $scope.chartPoints.push($scope.points);
-    
-    x = d3.time.scale().range([0, $scope.graph.width]);  
-    y = d3.scale.linear().range([$scope.graph.height, 0]);
+
+    var vis = d3.select('#visualisation'),
+    x = d3.scale.linear().range([ $scope.graph.MARGINS.left, $scope.graph.width - $scope.graph.MARGINS.right]);  
+    y = d3.scale.linear().range([$scope.graph.height - $scope.graph.MARGINS.top, $scope.graph.MARGINS.bottom]);
     
     x.domain(d3.extent($scope.points, function(d) {return d.x}));  
     y.domain(d3.extent($scope.points, function(d) {return d.y}));
     
-    $scope.line = d3.svg.line()
+    $scope.lineFunc = d3.svg.line()
       .x(function(d) {return x(d.x);})
-      .y(function(d) {return y(d.y);});
+      .y(function(d) {return y(d.y);})
+      .interpolate('linear');
+      
 
+      xAxis = d3.svg.axis()
+      .scale(x)
+      .tickSize(5)
+      .tickSubdivide(true),
+    yAxis = d3.svg.axis()
+      .scale(y)
+      .tickSize(5)
+      .orient('left')
+      .tickSubdivide(true);
+
+      vis.append('svg:g')
+  .attr('class', 'x axis')
+  .attr('transform', 'translate(0,' + ($scope.graph.height - $scope.graph.MARGINS.bottom) + ')')
+   .call(xAxis);
+
+vis.append('svg:g')
+  .attr('class', 'y axis')
+  .attr("y", 6)
+  .attr("dy", "0.71em")
+  .attr("text-anchor", "end")
+  .text("Price ($)")
+  .attr('transform', 'translate(' + ($scope.graph.MARGINS.left) + ',0)')
+  .call(yAxis);
+/*
+  for(let l in $scope.chartPoints)
+  {
+    vis.append('svg:path')
+    .attr('d', $scope.lineFunc(l.points))
+    .attr('stroke', l.color)
+    .attr('stroke-width', 2)
+    .attr('fill', 'none');
+  }
+  */
+ $scope.chartPoints.forEach(function(l) {
+    vis.append('svg:path')
+    .attr('d', $scope.lineFunc(l.points ? l.points : []))
+    .attr('stroke', l.color)
+    .attr('stroke-width', 2)
+    .attr('fill', 'none');
+  });
+
+/*
+  vis.append('svg:path')
+  .attr('d', $scope.lineFunc($scope.points))
+  .attr('stroke', 'blue')
+  .attr('stroke-width', 2)
+  .attr('fill', 'none');
+  */
     $scope.event = {
         name: 'Angular Boot Camp',
         date: '1/1/2018',
@@ -103,7 +197,7 @@ app.controller('EventController', function EventController($scope)
     };
 
     $scope.drawChart = function(){
-        var data = $scope.data;
+        var data = $scope.points;
         var svgWidth = 600, svgHeight = 400;
         var margin = { top: 20, right: 20, bottom: 30, left: 50 };
         var width = svgWidth - margin.left - margin.right;
@@ -116,7 +210,7 @@ app.controller('EventController', function EventController($scope)
             .attr("transform", 
             "translate(" + margin.left + "," + margin.top + ")");
 
-            var x = d3.scaleLinear().rangeRound([0, width]);
+            var x = d3.scaleTime().rangeRound([0, width]);
             var y = d3.scaleLinear().rangeRound([height, 0]);
             
             var line = d3.line()
@@ -130,6 +224,25 @@ app.controller('EventController', function EventController($scope)
             .call(d3.axisBottom(x))
             .select(".domain")
             .remove();
+
+                        g.append("g")
+            .call(d3.axisLeft(y))
+            .append("text")
+            .attr("fill", "#000")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", "0.71em")
+            .attr("text-anchor", "end")
+            .text("Price ($)");
+
+                    g.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", line);
 
     }
 });
